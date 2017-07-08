e9fa27856e750f8cf63980eb3b659692fbf25746
sgajjala <sgajjala@STHREE-DEV>
2017-07-08 14:27:50 +0000

    Initial checkin for database

---
+++ Sthree database/Sthree database/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[FirstName] NVARCHAR(10) NOT NULL PRIMARY KEY, 
+    [LastName] NVARCHAR(10) NULL, 
+    [UserName] NVARCHAR(10) NOT NULL, 
+    [Password] NVARCHAR(15) NOT NULL, 
+    [Gender] BIT NOT NULL, 
+    [Email] NVARCHAR(30) NOT NULL, 
+    [PhoneNumber] NVARCHAR(15) NOT NULL
+)
